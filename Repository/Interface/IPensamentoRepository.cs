606c37fe93247193c3e6408e2c5cc2cc6f80480d
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-16 12:41:06 -0300

    feat:Adicionar interface repository pensamento

---
+++ Repository/Interface/IPensamentoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Infrastructure.Interface
+{
+    public interface IPensamentoRepository
+    {
+        Task<bool> AdicionarPensamento(Pensamentos pensamentos);
+        Task<bool> RemoverPensamento(int id);
+        Task<bool> AtualizarPensamento(Pensamentos pensamentos);
+        Task<bool> RetornarPensamento();
+
+
+    }
+}
