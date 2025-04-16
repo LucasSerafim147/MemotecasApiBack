2bf9b25757ff1af40e0411fd2a7f2481420db211
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-16 13:36:54 -0300

    feat: criar profile Pensamentos

---
+++ Application/Profiles/PensamentosProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Models;
+
+namespace Application.Profiles 
+{
+    public class PensamentosProfile : Profile
+    {
+        public PensamentosProfile() {
+            CreateMap<PensamentosDto, Pensamentos>();
+
+        }
+
+    }
+}
