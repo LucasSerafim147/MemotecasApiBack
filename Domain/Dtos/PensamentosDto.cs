6a0e72999da129f7557e697cbf0b4e877af0e9dd
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-16 13:22:57 -0300

    feat: criacao dto e profile

---
+++ Domain/Dtos/PensamentosDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos
+{
+   public class PensamentosDto
+    {
+        public string? Pensamento { get; set; }
+        public string? Autor { get; set; }
+        public int Modelos { get; set; }
+    }
+}
